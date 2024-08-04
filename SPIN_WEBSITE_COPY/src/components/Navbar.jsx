95e2e601a912a61f12c1546c0783c3e648de79ea
Anderson Lopez <52222621+TheGodFatherpte@users.noreply.github.com>
2024-08-03 19:02:38 -0500

    Nav updated

--- SPIN_WEBSITE_COPY/src/components/Navbar.jsx
+++ SPIN_WEBSITE_COPY/src/components/Navbar.jsx
@@ -3,8 +3,27 @@ import { logo } from '../assets'
 
 
 const Navbar = () => (
-    <nav className=' w-full flex py-6 justify-between items-center navbar'>
+    <nav  className=' w-full flex py-6 justify-between items-center navbar'>
       <img id="logo" src={logo} alt = "SPIN" className="w-[124px] h-[50px]"/> 
+
+      <div id = "navbar" className='all'>
+        <div className='lefter'>
+            <div className='text'> Combos </div>
+        </div>
+        <div className='left'>
+          <div className='text'> Menu </div>
+        </div>
+        <div className='center'>
+          <div className='explainer'><span> Ordena ahora </span></div>
+          <div className='text'>Acerca de nosotros</div>
+        </div>
+        <div className='right'>
+          <div className='text'>Nuestro local</div>
+        </div>
+        <div className='righter'>
+          <div className='text'>Contacto</div>
+        </div>
+      </div>
     </nav>
     
   )//prueba
